dc54695f08ab05b0d92bbd92180df8d059d21dd6
evandrocoan <evandrocoan@hotmail.com>
2016-12-15 17:21:15 -0200

    Fixed the broken compiler shell script AmxxPawn.sh

--- AmxxPawn.sh
+++ AmxxPawn.sh
@@ -5,23 +5,18 @@
 #
 # Where is your compiler?
 # Example: "F:/SteamCMD/steamapps/common/Half-Life/czero/addons/amxmodx/scripting/amxxpc.exe"
-#
 AMXX_COMPILER_PATH="amxxpc.exe"
 
 # Where put the '.amxx' compiled file?
 # Example: "/SteamCMD/steamapps/common/Half-Life/czero/addons/amxmodx/plugins"
-#
 AMXX_OUTPUT_PLUGIN_FOLDER="F:/SteamCMD/steamapps/common/Half-Life/czero/addons/amxmodx/plugins"
 
-# Example: $1="F:/SteamCMD/steamapps/common/Half-Life/czero/addons/my_plugin.sma" and $2="my_plugin"
-#
+# Example: $1="F:/SteamCMD/steamapps/common/Half-Life/czero/addons/my_plugin.sma"
 FILE_PATH=$1
 
 # Example: $2="my_plugin"
-#
 PLUGIN_BASE_FILE_NAME=$2
 
 # To call the compiler to compile the plugin to the output folder $AMXX_OUTPUT_PLUGIN_FOLDER
-#
-$AMXX_COMPILER_PATH -o$AMXX_OUTPUT_PLUGIN_FOLDER/$PLUGIN_BASE_FILE_NAME.amxx $FILE_PATH
+./$AMXX_COMPILER_PATH -o"$AMXX_OUTPUT_PLUGIN_FOLDER/$PLUGIN_BASE_FILE_NAME.amxx" "$FILE_PATH"
 
