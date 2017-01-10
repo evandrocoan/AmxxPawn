c11f056a08c1ba0d9a04c212a066e34ca9abc76b
evandrocoan <evandrocoan@hotmail.com>
2017-01-10 16:28:45 -0200

    Added a mirror binary copy on AmxxPawn.sh

--- AmxxPawn.sh
+++ AmxxPawn.sh
@@ -17,6 +17,10 @@ FILE_PATH=$1
 # Example: $2="my_plugin"
 PLUGIN_BASE_FILE_NAME=$2
 
+PLUGIN_BINARY_FULL_PATH=$AMXX_OUTPUT_PLUGIN_FOLDER/$PLUGIN_BASE_FILE_NAME.amxx
+
 # To call the compiler to compile the plugin to the output folder $AMXX_OUTPUT_PLUGIN_FOLDER
-./$AMXX_COMPILER_PATH -o"$AMXX_OUTPUT_PLUGIN_FOLDER/$PLUGIN_BASE_FILE_NAME.amxx" "$FILE_PATH"
+./$AMXX_COMPILER_PATH -o"$PLUGIN_BINARY_FULL_PATH" "$FILE_PATH"
+
+cp "$PLUGIN_BINARY_FULL_PATH" "F:/SteamCMD/steamapps/common/Half-Life/cstrike/addons/amxmodx/plugins/"
 
