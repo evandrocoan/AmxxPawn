31c903eff4b1945a2114473f9c56b1ba8286068d
evandrocoan <evandrocoan@hotmail.com>
2017-01-20 22:11:14 -0200

    Fixed AmxxPawn.sh not reading the compiler from the path variable.

--- AmxxPawn.sh
+++ AmxxPawn.sh
@@ -156,7 +156,7 @@ PLUGIN_BASE_FILE_NAME=$2
 PLUGIN_BINARY_FULL_PATH=$AMXX_OUTPUT_PLUGIN_FOLDER/$PLUGIN_BASE_FILE_NAME.amxx
 
 # To call the compiler to compile the plugin to the output folder $AMXX_OUTPUT_PLUGIN_FOLDER
-./$AMXX_COMPILER_PATH -o"$PLUGIN_BINARY_FULL_PATH" "$FILE_PATH"
+"$AMXX_COMPILER_PATH" -o"$PLUGIN_BINARY_FULL_PATH" "$FILE_PATH"
 
 cp "$PLUGIN_BINARY_FULL_PATH" "F:/SteamCMD/steamapps/common/Half-Life/cstrike/addons/amxmodx/plugins/"
 
